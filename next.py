c586ebdb90aa29da4dbd91595ff346902a6f4c28
Jordan Burgess <jordanburgess@gmail.com>
2014-05-12 21:36:07 +0100

    working, mostly cleaned up

--- next.py
+++ next.py
@@ -1,62 +1,55 @@
 #!/usr/bin/python
 
-import os, argparse
+import argparse
 from sys import stdin
 import datetime
-import fileinput
 
 def main(args):
-    print 'Input file is', args.input
 
-time_mins, time_hours = 46, 23 
-start_time = datetime.datetime.now().replace(hour=time_hours, minute=time_mins)
-print start_time
-print "Start time: {0:%H}:{0:%M} {1}".format(start_time, "today")
+    start_time = datetime.datetime.now()
+    if args.time:
+        start_hour, start_minute = args.time.split(':')
+        start_time = start_time.replace(hour=int(start_hour), minute=int(start_minute))
+    print "Start time: {0:%H}:{0:%M} {1}".format(start_time, "today")
 
 
-for line in fileinput.input():
-    mins, hours, program = line.strip().split(' ')
-    day_offset = 0
+    for line in stdin:
 
-    if hours == '*':
-        if mins == '*' or int(mins) > start_time.minute:
-            hours = start_time.hour
+        day_offset = 0
+        cron_minute, cron_hour, program = line.strip().split(' ')
+
+        if cron_minute == '*':
+            if cron_hour == '*' or int(cron_hour) == start_time.hour:
+                next_event_minute = start_time.minute
+            else:
+                next_event_minute = 0
         else:
-            hours = start_time.hour + 1 #consider wrap around at 23 + horus
-            if hours >= 24:
-                hours = hours%24
+            next_event_minute = int(cron_minute)
+
+
+        if cron_hour == '*':
+            if next_event_minute >= start_time.minute:
+                next_event_hour = start_time.hour
+            else:
+                day_offset, next_event_hour = divmod(start_time.hour + 1, 24)
+        else:
+            next_event_hour = int(cron_hour)
+            if next_event_hour < start_time.hour:
                 day_offset += 1
+        
+        next_event = start_time.replace(
+            day=start_time.day + day_offset,
+            hour=next_event_hour,
+            minute=next_event_minute
+        )
 
-    else:
-        hours = int(hours)
+        event_day = "tomorrow" if day_offset else "today"
 
-    if mins == '*':
-        mins = start_time.minute if hours == start_time.hour else 0
-    else: 
-        mins = int(mins)
-
-    event_time = start_time.replace(hour=hours, minute=mins)
-    event_time = event_time + datetime.timedelta(days = day_offset)
-    
-
-    if event_time.date() == start_time.date():
-        event_day = "today"
-    else:
-        event_day = "tomorrow"
-
-    print "{0:%H}:{0:%M} {1} - {2}".format(event_time, event_day, program)
+        print "{0:%H}:{0:%M} {1} - {2}".format(next_event, event_day, program)
 
 
-
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser(description='Simulate an alien invasion')
-#     parser.add_argument(
-#         '-number', type=int, help='Number of alien invaders', default=20)
-#     parser.add_argument(
-#         "--input", help="Input file listing cities and their roads",
-#         default="world_map_small.txt")
-#     parser.add_argument(
-#         "--output", help="Output file for remaining cities post-invasion",
-#         default="world_map_small_post_invasion.txt")
-#     args = parser.parse_args()
-#     main(args)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Calculate the next time a cron job will execute')
+    parser.add_argument('time', nargs='?', help="HH:MM - Time from which to calculate next cron")
+    args = parser.parse_args()
+    main(args)
